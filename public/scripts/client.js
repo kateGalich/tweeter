62b682604b0b86b8ef184270dcc4e9b6ed0a3709
Kate Galich <kate.galich@outlook.com>
2022-10-12 05:28:11 -0400

    display tweets by js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,63 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$(document).ready(function() {
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd"
+      },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
+
+  const createTweetElement = function(tweetObj) {
+    const $tweet = $(`<article class="tweet">
+    <header>
+      <span class = "author">
+        <img src="${tweetObj.user.avatars}">
+        <span> ${tweetObj.user.name}</span>
+      </span>
+      <span class ="nick"> ${tweetObj.user.handle}</span>
+    </header>
+    <p class="message"> ${tweetObj.content.text}</p>
+    <footer>
+      <div>${tweetObj.created_at} days ago</div>
+      <span>
+        <i class="fa-solid fa-flag"></i>
+        <i class="fa-solid fa-retweet"></i>
+        <i class="fa-solid fa-heart"></i>
+      </span>
+    </footer>
+  </article>`);
+
+    return $tweet;
+  };
+
+  const renderTweets = function(tweetsArr) {
+    tweetsArr.forEach(tweet => {
+      const $tweet = createTweetElement(tweet);
+      $('#all-tweets').append($tweet);
+    });
+  };
+
+  renderTweets(data);
+})
 
